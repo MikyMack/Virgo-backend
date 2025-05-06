768507f31160ef47d6d39497dc09de602044153c
micky mack <jijomonjs@gmail.com>
2025-05-06 17:50:22 +0530

    back

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 mongoose.connect(process.env.MONGO_URI)
 
